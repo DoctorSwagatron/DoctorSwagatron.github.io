21452246d26288c4c3337eb98cdd6f824036699e
doctorswagatron <doctorswagatron@byui.edu>
2020-10-24 19:19:58 -0700

    pushing changes to js code- footwear

--- scripts/footwear.js
+++ scripts/footwear.js
@@ -2,15 +2,15 @@ function footwear(){
 //inputs 
 let weather = document.getElementById("weather").value;
 //processing
-if (weather == "hot") {
-    decision = "sandals";
+if (weather == "Hot") {
+    decision = "Sandals";
    //  block of code to be executed if condition1 is true
-} else if (weather == "rain") {
-    decision = "galoshes";
-} else if (weather == "snow") {
-    decision = "boots";
+} else if (weather == "Rain") {
+    decision = "Galoshes";
+} else if (weather == "Snow") {
+    decision = "Boots";
 } else {
-    decision = "shoes";
+    decision = "Shoes";
 }
 
 //outputs      
